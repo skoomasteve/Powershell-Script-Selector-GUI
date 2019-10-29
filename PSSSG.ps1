b866fc78d7814a0c482576ccaa4b4232a547c0ff
skoomasteve <43890114+skoomasteve@users.noreply.github.com>
2019-10-29 10:50:38 -0500

    Add files via upload

---
+++ PSSSG.ps1
@@ -0,0 +1,34 @@
+﻿$userresponseend=[System.Windows.MessageBox]::Show("The following dialogue will allow you to select the application you would like to run", 'Script Selector - Application Selector','ok')
+if ($UserResponseend -eq "ok" ) 
+{
+
+#Yes activity
+
+} 
+
+else 
+
+{ 
+
+exit
+
+} 
+
+
+$scriptPath = "C:\PSSSG\"
+msg $env:UserName "Select the Application you would like to run"
+$Selected = Get-ChildItem -Path $scriptpath | select name,lastwritetime,creationtime | Out-GridView -Title "Script Selector - Select the desired application" -PassThru | format-wide
+
+if($null -eq $Selected) 
+{
+
+exit
+}
+else 
+{
+'OK was pressed, $Selected contains what was chosen'
+#$scriptpath +="$selected"
+& $selected
+}
+
+
